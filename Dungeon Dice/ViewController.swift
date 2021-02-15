c3c1b7a154b2fab5710bde9dadeb76ef863df688
Nipuni Obe <obe@gmail.com>
2021-02-15 18:53:31 -0500

    dungeon dice in class 2/15

--- Dungeon Dice/ViewController.swift
+++ Dungeon Dice/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        resultLabel.text = ""
     }
 
-
+    @IBAction func diceButtonPressed(_ sender: UIButton) {
+        let diceRoll = Int.random(in: 1...sender.tag)
+        resultLabel.text = "You rolled a \(sender.tag)-sided dice and got a \(diceRoll)"
+    }
+    
 }
 
